ed5b8496f8ba8b46b19b36c1cbc8736c68ccf2ab
Dabiny <lina917@naver.com>
2022-04-27 14:38:16 +0900

    백준문제풀이

---
+++ baekjoon/baekjoon/10988.cpp
@@ -0,0 +1,26 @@
+//
+//  10988.cpp
+//  baekjoon 팰린드롬인지 확인하기
+//
+//  Created by Dabin Kim on 2022/04/27.
+//
+
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//
+//string str1, str2;
+//
+//int main()
+//{
+//    cin >> str1; //baekjoon
+//    str2 = str1;
+//
+//    reverse(str1.begin(), str1.end());
+//
+//    if (str1.substr() == str2.substr())
+//        cout << 1;
+//    else
+//        cout << 0;
+//}
