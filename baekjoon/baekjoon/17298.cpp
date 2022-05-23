e5cc9a243995bd6c284e1705052faf45d55c7fd7
Dabiny <lina917@naver.com>
2022-05-23 14:13:32 +0900

    no message

--- baekjoon/baekjoon/17298.cpp
+++ baekjoon/baekjoon/17298.cpp
@@ -1,51 +1,26 @@
-//#include <iostream>
-//#include <queue>
-//#include <string.h>
-//using namespace std;
-//int n, num;
-//vector<pair<int, int>> A;
-//vector<int> v;
-//struct compare
-//{
-//    bool operator() (pair<int, int>& a, pair<int, int>& b)
-//    {
-//        if (a.first < b.first)
-//        {
-//            return b.second < a.second;
-//        }
-//        else
-//        {
-//            return  -1;
-//        }
-//    }
-//};
-//int main()
-//{
-//    cin >> n;
-//    for (int i = 0; i < n; i++)
-//    {
-//        cin >> num;
-//        A.push_back({num, i});
-//    }
-//    
-//    priority_queue<pair<int, int>,vector<pair<int,int>>, compare> pq;
-//    
-//    for(int i = 0; i < A.size(); i++)
-//    {
-//        int nge = i;
-//        if (nge == A.size() - 1){
-//            v.push_back(-1);
-//            break;
-//        }
-//        for (int j = 1; j <= A.size(); j++)
-//        {
-//            pq.push({A[j]});
-//        }
-//        v.push_back(pq.top().first);
-//        pq.empty();
-//    }
-//    
-//    for (auto i : v)
-//        cout << i << " ";
-//}
-//
+#include <iostream>
+#include <stack>
+using namespace std;
+int n, a[100'000'4], ret[100'000'4];
+stack<int> s;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    memset(ret, -1, sizeof(ret));
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        while(s.size() && a[s.top()] < a[i])
+        {
+            ret[s.top()] = a[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << ret[i] << " ";
+    }
+}
