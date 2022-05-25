5022d6953f6bd314c36b5db715cde50353946c79
Dabiny <lina917@naver.com>
2022-05-25 17:34:33 +0900

    no message

---
+++ baekjoon/baekjoon/1189.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int r, c, k, visited[6][6], cnt;
+char a[6][6];
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+int go (int y, int x)
+{
+    if (y == 0 && x == c - 1)
+    {
+        if (k == visited[y][x])
+            return 1;
+        return 0;
+    }
+    int n = 0; // 이걸 세우는 기준을 모르겠음
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        
+        if (ny < 0 || ny >= r || nx < 0 || nx >= c || visited[ny][nx] || a[ny][nx] == 'T')
+            continue;
+        visited[ny][nx] = visited[y][x] + 1;
+        for(int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                cout << visited[i][j] << " ";
+            }
+            cout << "\n";
+        }
+        cout <<"\n";
+        n += go(ny, nx);
+        visited[ny][nx] = 0; //다시 세팅해주기?
+    }
+    return n;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> r >> c >> k;
+    
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    
+    visited[r-1][0] = 1;
+    cnt = go(r-1, 0);
+    cout << cnt;
+}
--- baekjoon/baekjoon/17298.cpp
+++ baekjoon/baekjoon/17298.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <stack>
-using namespace std;
-int n, a[100'000'4], ret[100'000'4];
-stack<int> s;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    memset(ret, -1, sizeof(ret));
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-        while(s.size() && a[s.top()] < a[i])
-        {
-            ret[s.top()] = a[i];
-            s.pop();
-        }
-        s.push(i);
-    }
-    for(int i = 0; i < n; i++)
-    {
-        cout << ret[i] << " ";
-    }
-}
+//#include <iostream>
+//#include <stack>
+//using namespace std;
+//int n, a[100'000'4], ret[100'000'4];
+//stack<int> s;
+//int main()
+//{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL); cout.tie(NULL);
+//    cin >> n;
+//    memset(ret, -1, sizeof(ret));
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin >> a[i];
+//        while(s.size() && a[s.top()] < a[i])
+//        {
+//            ret[s.top()] = a[i];
+//            s.pop();
+//        }
+//        s.push(i);
+//    }
+//    for(int i = 0; i < n; i++)
+//    {
+//        cout << ret[i] << " ";
+//    }
+//}
