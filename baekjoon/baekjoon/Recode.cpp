990a9c34a6efa6eaa558513c8b0fe497e806826a
Dabiny <lina917@naver.com>
2022-05-31 14:21:31 +0900

    no message

--- baekjoon/baekjoon/Recode.cpp
+++ baekjoon/baekjoon/Recode.cpp
@@ -1,73 +1,54 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//using namespace std;
-//const int dy[4] = {-1, 0, 1, 0};
-//const int dx[4] = {0, 1, 0, -1};
-//
-//int n, m, a[10][10];
-//bool visited[10][10];
-//vector<pair<int, int>> v;
-//void go (int y, int x)
-//{
-//    if (a[y][x] == 1 || visited[y][x])
-//        return;
-//    visited[y][x] = 1;
-//    for (int i = 0; i < 4; i++)
-//    {
-//        int ny = y + dy[i];
-//        int nx = x + dx[i];
-//
-//        if (ny < 0 || ny >= n || nx < 0 || ny >= m || visited[ny][nx] || a[ny][nx] == 1)
-//            continue;
-//        go(ny, nx);
-//    }
-//    return;
-//}
-//
-//int solve()
-//{
-//    memset(visited, 0, sizeof(visited));
-//    for(int i = 0; i < n; i++)
-//        for (int j = 0; j < m; j++)
-//        {
-//            if (a[i][j] == 2)
-//                go(i,j);
-//        }
-//
-//    int cnt = 0;
-//
-//    for (int i = 0; i < n; i++)
-//        for (int j = 0; j < m; j++)
-//        {
-//            if (a[i][j] == 0 && !visited[i][j])
-//                cnt++;
-//        }
-//    return cnt;
-//}
-//
-//int main()
-//{
-//    cin >> n >> m;
-//
-//    for (int i = 0; i < n; i++)
-//        for (int j = 0; j < m; j++)
-//        {
-//            cin >> a[i][j];
-//            if (!a[i][j])
-//                v.push_back({i,j});
-//        }
-//
-//    int ans = 0;
-//
-//    for (int i = 0; i < v.size(); i++)
-//        for (int j = 0; j < i; j++)
-//            for (int k = 0; j < k; k++)
-//            {
-//                a[v[i].first][v[i].second] = a[v[j].first][v[j].second] = a[v[k].first][v[k].second] = 1;
-//                max (ans, solve());
-//                a[v[i].first][v[i].second] = a[v[j].first][v[j].second] = a[v[k].first][v[k].second] = 0;
-//            }
-//
-//    cout << ans << " ";
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+int t, m, n, k, a[54][54], y, x, cnt;
+bool visited[54][54];
+vector<int> ans;
+void dfs (int y, int x)
+{
+    if (visited[y][x])
+        return;
+    visited[y][x] = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m)
+            continue;
+        if (visited[ny][nx] && a[ny][nx] == 0)
+            continue;
+        dfs(ny, nx);
+    }
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> t;
+    while (t--)
+    {
+        memset(a, 0, sizeof(a));
+        memset(visited, 0, sizeof(visited));
+        cin >> m >> n >> k;
+        for (int i = 0; i < k; i++)
+        {
+            cin >> x >> y;
+            a[y][x] = 1;
+        }
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+            {
+                if (!visited[i][j] && a[i][j] == 1)
+                {
+                    dfs(i,j);
+                    cnt++;
+                }
+            }
+        ans.push_back(cnt);
+        cnt = 0;
+    }
+}
--- baekjoon/baekjoon/개념강의_예제_당근마킷_승원이.cpp
+++ baekjoon/baekjoon/개념강의_예제_당근마킷_승원이.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
-int n, m, sy, sx, ey, ex, a[104][104], visited[104][104], y, x;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> m;
-    cin >> sy >> sx;
-    cin >> ey >> ex;
-    
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            cin >> a[i][j];
-    
-    visited[sy][sx] = 1;
-    queue<pair<int,int>> q;
-    q.push({sy,sx});
-    while (q.size())
-    {
-        tie(y, x) = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx])
-                continue;
-            if (a[ny][nx] == 0)
-                continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny,nx});
-        }
-    }
-    
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//const int dy[4] = {-1, 0, 1, 0};
+//const int dx[4] = {0, 1, 0, -1};
+//int n, m, sy, sx, ey, ex, a[104][104], visited[104][104], y, x;
+//int main()
+//{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL); cout.tie(NULL);
+//    cin >> n >> m;
+//    cin >> sy >> sx;
+//    cin >> ey >> ex;
+//
+//    for (int i = 0; i < n; i++)
+//        for (int j = 0; j < m; j++)
+//            cin >> a[i][j];
+//
+//    visited[sy][sx] = 1;
+//    queue<pair<int,int>> q;
+//    q.push({sy,sx});
+//    while (q.size())
+//    {
+//        tie(y, x) = q.front();
+//        q.pop();
+//        for (int i = 0; i < 4; i++)
+//        {
+//            int ny = y + dy[i];
+//            int nx = x + dx[i];
+//            if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx])
+//                continue;
+//            if (a[ny][nx] == 0)
+//                continue;
+//            visited[ny][nx] = visited[y][x] + 1;
+//            q.push({ny,nx});
+//        }
+//    }
+//
+//}
