9aebac6514943ac21cff5a0c98305f7db8ed2898
Dabiny <lina917@naver.com>
2022-06-13 13:36:58 +0900

    no message

--- baekjoon/baekjoon/practice_2.cpp
+++ baekjoon/baekjoon/practice_2.cpp
@@ -1,62 +1,63 @@
 //#include <iostream>
-//#include <map>
+//#include <vector>
 //#include <algorithm>
 //using namespace std;
-//const int INF = 987654321;
-//int a[104][104], ret = INF, n = 10;
-//map<int, int> mp;
-//void draw (int y, int x, int cnt, int value)
+//const int dy[4] = {-1, 0, 1, 0};
+//const int dx[4] = {0, 1, 0, -1};
+//int n, m, a[10][10];
+//bool virus[10][10];
+//vector<pair<int, int>> v;
+//void dfs (int y, int x)
 //{
-//    for (int i = y; i < y + cnt; i++)
-//        for (int j = x; j < x + cnt; j++)
-//            a[i][j] = value;
-//}
-//bool check (int y, int x, int cnt)
-//{
-//    if (y + cnt > n || x + cnt > n)
-//        return false;
-//    for (int i = y; i < y + cnt; i++)
-//        for (int j = x; j < x + cnt; j++)
-//            if (a[i][j] == 0)
-//                return false;
-//    return true;
-//}
-//void dfs (int y, int x, int cnt)
-//{
-//    if (cnt >= ret) //ret은 마지막을 출력할 무언가
-//        return;
-//    if (x == n)
-//        dfs(y + 1, 0, cnt);
-//    if (y == n) //y == n이다? 필드가 끝난거임
+//    for (int i = 0; i < 4; i++)
 //    {
-//        ret = min(cnt, ret);
-//        return;
-//    }
-//    if (a[y][x] == 0)
-//    {
-//        dfs (y, x + 1, cnt);
-//        return;
-//    }
-//    for (int _size = 5; _size >= 1; _size--) //색종이는 큰것부터 붙이기
-//    {
-//        if (mp[_size] == 5) //5개 다쓰면 컨티뉴
+//        int ny = y + dy[i];
+//        int nx = x + dx[i];
+//        if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 1 || virus[ny][nx])
 //            continue;
-//        if (check(y, x, _size)) //이 색종이를 과연 붙일수 있냐없냐? 있으면
+//        if (virus[ny][nx] == 0 && a[ny][nx] == 0)
 //        {
-//            mp[_size]++; //색종이 ++
-//            draw(y, x, _size, 0); //색칠하기
-//            dfs (y, x + _size, cnt + 1); //cnt + 1해주면서 다시 dfs돌기
-//            draw (y, x, _size, 1); //다시 떼기
-//            mp[_size]--;
+//            virus[ny][nx] = 1;
+//            dfs(ny, nx);
 //        }
 //    }
+//    return;
+//}
+//int solve ()
+//{
+//    memset (virus, 0, sizeof(virus));
+//    for (int i = 0; i < n; i++)
+//        for (int j = 0; j < m; j++)
+//            if (a[i][j] == 2)
+//                dfs(i,j);
+//    int cnt = 0;
+//    for (int i = 0; i < n; i++)
+//        for (int j = 0; j < m; j++)
+//           if (virus[i][j] == 0 && a[i][j] == 0)
+//               cnt++;
+//    return cnt;
 //}
 //int main()
 //{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL); cout.tie(NULL);
+//    cin >> n >> m;
 //    for (int i = 0; i < n; i++)
-//        for (int j = 0; j < n; j++)
+//        for (int j = 0; j < m; j++)
+//        {
 //            cin >> a[i][j];
-//    dfs(0,0,0);
-//    cout << (ret == INF ? -1 : ret) << "\n";
-//    return 0;
+//            if (!a[i][j])
+//                v.push_back({i,j});
+//        }
+//    
+//    int ans = 0;
+//    
+//    for (int i = 0; i < v.size(); i++)
+//        for (int j = 0; j < i; j++)
+//            for (int k = 0; k < j; k++)
+//            {
+//                a[v[i].first][v[i].second] = a[v[j].first][v[j].second] = a[v[k].first][v[k].second] = 1;
+//                ans = max(ans, solve());
+//                a[v[i].first][v[i].second] = a[v[j].first][v[j].second] = a[v[k].first][v[k].second] = 0;
+//            }
 //}
