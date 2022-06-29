306b92757ac900f08468d6a1e9401c7736cf1819
Dabiny <lina917@naver.com>
2022-06-29 14:28:57 +0900

    no message

--- baekjoon/baekjoon/mordernCpp.cpp
+++ baekjoon/baekjoon/mordernCpp.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+int divide(int a, int b)
+{
+    return a / b;
+}
+float divide(float a, float b)
+{
+    return floor(a / b);
+}
 int main()
 {
-    int n = 0;
-    n = 7 + 3, 7 * 9;
-    cout << n << endl;
+    int x = 5, y = 2;
+    float n = 5.0, m = 2.0;
+    cout << divide(x, y) << endl;
+    cout << divide(n, m) << endl;
+
 }
 
 
