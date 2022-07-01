26fcf372c3e736d493a8be2b4b663632acd9a6d7
Dabiny <lina917@naver.com>
2022-07-01 17:30:07 +0900

    no message

--- baekjoon/baekjoon/mordernCpp.cpp
+++ baekjoon/baekjoon/mordernCpp.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int divide(int a, int b)
-{
-    return a / b;
-}
-float divide(float a, float b)
-{
-    return floor(a / b);
-}
-int main()
-{
-    int x = 5, y = 2;
-    float n = 5.0, m = 2.0;
-    cout << divide(x, y) << endl;
-    cout << divide(n, m) << endl;
-
-}
+//#include <cassert>
+//double square_root(double x)
+//{
+//    check_somehow( x >= 0);
+//    assert(result >= 0.0);
+//    return result;
+//}
 
 
